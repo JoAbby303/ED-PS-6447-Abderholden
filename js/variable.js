d363bcefc746e4dd6831596919c79c8223d24dc5
JoAbby303 <46548049+JoAbby303@users.noreply.github.com>
2019-06-04 19:36:09 -0600

    Add files via upload

---
+++ js/variable.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+var element = document.getElementById("viewportfolio");
+console.log(element);
+var element = document.getElementById("viewhome");
+console.log(element);
+var element = document.getElementById("viewresume");
+console.log(element);
