4d7f21e780602682d57652cb10d0081941e21259
JoAbby303 <46548049+JoAbby303@users.noreply.github.com>
2019-06-04 19:36:27 -0600

    Delete variable.js

--- variable.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World!");
-var element = document.getElementById("viewportfolio");
-console.log(element);
-var element = document.getElementById("viewhome");
-console.log(element);
-var element = document.getElementById("viewresume");
-console.log(element);
