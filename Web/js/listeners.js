6336ed98dd498fe8b1cee377c9dc14d220b274d5
JoAbby303 <46548049+JoAbby303@users.noreply.github.com>
2019-07-04 15:31:21 -0600

    Delete listeners.js

--- Web/js/listeners.js
+++
@@ -1,9 +0,0 @@
-viewportfolio.addEventListener("click",function(){
-    router("view_portfolio");
-});
-viewhome.addEventListener("click", function(){
-   router("grid"); 
-});
-viewresume.addEventListener("click", function(){
-    router("view_resume");
-});
